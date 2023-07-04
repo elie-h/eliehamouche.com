ba5da0b56d7f4cb8c957bc504039373165cf3473
Elie Hamouche <elie.hamouche@gmail.com>
2023-07-04 09:58:56 +0100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-bold text-2xl mb-8 tracking-tighter">Home</h1>
+      <h1 className="font-bold text-2xl mb-8 tracking-tighter">my portfolio</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        Welcome, I'm still working on this :)
+        This is your new portfolio.
       </p>
     </section>
   );
