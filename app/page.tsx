1798357d002a5ba3e859850c74883fc7ef6a3970
Elie Hamouche <elie.hamouche@gmail.com>
2023-07-06 10:40:44 +0100

    assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-bold text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-bold text-2xl mb-8 tracking-tighter">Hi, I'm Elie</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        
+        
+        This is a space where I write about things I'm interested in.
+        You can read some of my <a href="/blogs">posts</a> where I mostly write about tech, or check out my <a href="/projects">projects</a> where I share some of my work.
+        or find me on socials: <a href="https://twitter.com/thehamouche">Twitter</a>
       </p>
     </section>
   );
---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <section>
+        <h1 className="font-bold text-2xl mb-8 tracking-tighter">Coming soon!</h1>
+      </section>
+    );
+  }
+  
